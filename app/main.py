602070a63085a5a39c3b1824a3ff2fcd265ff568
Aanchal-Jagga <aanchaljagga2000@gmail.com>
2025-11-01 21:53:42 +0530

    main changes

--- app/main.py
+++ app/main.py
@@ -5,17 +5,17 @@ load_dotenv()
 from fastapi import FastAPI
 from app.routes.process_endpoint import router as  process_endpoint
 from app.routes.handwritten_routes import router as handwritten_routes
-from firebase_config import db  # Import Firestore client
-from firebase_admin import firestore
+# from firebase_config import db  # Import Firestore client
+# from firebase_admin import firestore
 
-from app.routes.auth_routes import router as auth_router
-from app.routes.user_routes import router as user_router
+# from app.routes.auth_routes import router as auth_router
+# from app.routes.user_routes import router as user_router
 from app.routes.math_ocr_endpoint import router as process_router
 from app.routes.symb import router as detect_symbol
 app = FastAPI(title="FlowGenix Backend")
 
-app.include_router(auth_router)
-app.include_router(user_router)
+# app.include_router(auth_router)
+# app.include_router(user_router)
 app.include_router(process_endpoint)
 app.include_router(handwritten_routes)
 
